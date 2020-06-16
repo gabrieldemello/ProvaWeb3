168fc41afc400cc6afcdedde3d4aaf0857608917
Gabriel de Mello e Silva Carneiro <35747250+gabrieldemello@users.noreply.github.com>
2020-06-16 18:45:25 -0300

    Initial Commit

---
+++ ProvaWeb3/ProvaWeb3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProvaWeb3.MvcApplication" Language="C#" %>
---
+++ ProvaWeb3/ProvaWeb3/Models/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProvaWeb3.Models
+{
+    public class Aluno
+    {
+        [Key]
+        public int Ra { get; set; }
+        public string Nome { get; set; }
+        public String Sexo { get; set; }
+        public int DataNasc { get; set; }
+        public string Email { get; set; }
+        public string Endereco { get; set; }
+        public string Curso { get; set; }
+        public int AnoInicial { get; set; }
+    }
+}
